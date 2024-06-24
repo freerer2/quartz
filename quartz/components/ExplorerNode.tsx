eab248f4253a33a704b603e84f41321ffb963640
LHK <30331140+freerer2@users.noreply.github.com>
2024-06-24 13:25:20 +0900

    ㅇ

--- quartz/components/ExplorerNode.tsx
+++ quartz/components/ExplorerNode.tsx
@@ -185,8 +185,22 @@ export function ExplorerNode({ node, opts, fullPath, fileData }: ExplorerNodePro
       {node.file ? (
         // Single file node
         <li key={node.file.slug}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="12"
+            viewBox="5 8 14 8"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="text-icon"
+          >
+            <polyline points="8 12 16 12"></polyline>
+          </svg>
           <a href={resolveRelative(fileData.slug!, node.file.slug!)} data-for={node.file.slug}>
-            - {node.displayName}
+            {node.displayName}
           </a>
         </li>
       ) : (
